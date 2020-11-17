5645e8db51c5cf1a7bc662971dd321faa3b968f3
yuezhilanyi <yuezhilanyi@hotmail.com>
2020-11-17 17:10:50 +0800

    chapter3 init

---
+++ code/chapter3/dbdriver/postgres/postgres.go
@@ -0,0 +1,24 @@
+package postgres 
+
+import (
+	"database/sql"
+	"database/sql/driver"
+	"errors"
+)
+
+// PostgresDriver provides our implementation for the 
+// sql package.
+type PostgresDriver struct{}
+
+// open provides a connection to the database.
+func (dr PostgresDriver) Open(string) (driver.Conn, error) {
+	return nil, errors.New("Unimplementd")
+}
+
+var d *PostgresDriver
+
+// init is called prior to main.
+func init() {
+	d = new(PostgresDriver)
+	sql.Register("postgres", d)
+}
---
+++ code/chapter3/wordcount/wordcount.go
@@ -0,0 +1,25 @@
+package main 
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+
+	"github.com/yuezhilanyi/goinaction/code/chapter3/words"
+)
+
+// main is the entry point for the application. 
+func main() {
+	filename := os.Args[1]
+
+	contents, err := ioutil.ReadFile(filename)
+	if err != nil {
+		fmt.Println(err)
+		return 
+	}
+
+	text := string(contents)
+
+	count := words.CountWords(text)
+	fmt.Printf("There are %d words in your text. \n", count)
+}
