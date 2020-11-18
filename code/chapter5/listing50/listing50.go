94e5924202b13a07927e99095475bacdf90c9647
yuezhilanyi <yuezhilanyi@hotmail.com>
2020-11-18 18:46:54 +0800

    update listing50/listing50.go

--- code/chapter5/listing50/listing50.go
+++ code/chapter5/listing50/listing50.go
@@ -1,46 +1,45 @@
-// Sample program to show how to embed a type into another type and 
-// the relationship between the inner and outer type. 
-package main 
+// Sample program to show how to embed a type into another type and
+// the relationship between the inner and outer type.
+package main
 
 import (
-    "fmt"
+	"fmt"
 )
 
-// user defines as user in the program. 
+// user defines as user in the program.
 type user struct {
-    name string 
-    email string 
+	name  string
+	email string
 }
 
-// notify implements as method that can be called via 
-// a value of type user. 
+// notify implements as method that can be called via
+// a value of type user.
 func (u *user) notify() {
-    fmt.Printf("Sending user email to %s<%s>\n",
-    u.name, 
-    u.email)
+	fmt.Printf("Sending user email to %s<%s>\n",
+		u.name,
+		u.email)
 }
 
 // admin represents an admin user with privileges.
 type admin struct {
-    user // Embdeded Type 
-    level string 
+	user  // Embdeded Type
+	level string
 }
 
-// main is the entry point for the application. 
+// main is the entry point for the application.
 func main() {
-    // Create an admin user. 
-    ad := admin{
-        user: user{
-            name: "john smith", 
-            email: "john@yahoo.com", 
-        }, 
-        level: "super",
-    }
+	// Create an admin user.
+	ad := admin{
+		user: user{
+			name:  "john smith",
+			email: "john@yahoo.com",
+		},
+		level: "super",
+	}
 
-    // We can access the inner type's method directly. 
-    ad.user.notify()
+	// We can access the inner type's method directly.
+	ad.user.notify()
 
-    // The inner type's method is promoted. 
-    ad.notify()
+	// The inner type's method is promoted.
+	ad.notify()
 }
-
